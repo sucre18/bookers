c526e26b0febdcec71f1b28d0e9796861ecb8f99
EC2 Default User <ec2-user@ip-172-31-82-237.ec2.internal>
2022-01-31 21:32:43 +0000

    [Start]2-11

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  get 'top' => 'homes#top'
+  root to: 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
